afcaa956e8db1a679381b54286e4a2174310aabc
Satyam <saytam.garg@goldeneagle.ai>
2025-02-26 20:07:51 +0530

    Search Thread Get API Creation

--- src/controllers/thread.controller.ts
+++ src/controllers/thread.controller.ts
@@ -26,3 +26,51 @@ export const getAllThreads = async (req: Request, res: Response) => {
     }
 };
 
+export const searchThreads = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const searchQuery = req.query.query as string;
+    const user = (req as any).user;
+
+    if (!user) {
+      res.status(400).json({ code: 400, message: "Invalid user" });
+      return;
+    }
+    if (!searchQuery) {
+      res.status(400).json({ code: 400, message: "Missing search query" });
+      return;
+    }
+
+    const threads = await prisma.thread.findMany({
+      where: {
+        aiOrgId: user.aiOrgId,
+        messages: {
+          some: {
+            content: {
+              contains: searchQuery,
+              mode: "insensitive",
+            },
+          },
+        },
+      },
+      include: {
+        messages: {
+          where: {
+            content: {
+              contains: searchQuery,
+              mode: "insensitive",
+            },
+          },
+        },
+      },
+    });
+
+    res.status(200).json({
+      code: 200,
+      data: { threads, TotalThreads: threads.length },
+      message: "Search completed successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ code: 500, message: "Error searching threads" });
+  }
+};
--- src/routes/thread.routes.ts
+++ src/routes/thread.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getAllThreads } from "../controllers/thread.controller";
+import { getAllThreads, searchThreads } from "../controllers/thread.controller";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.get("/", getAllThreads);
+router.get("/search", authMiddleware,searchThreads);
 
 export default router;
