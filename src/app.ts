a1f577ff43496164f9226479ec6de5679deec0ab
Anjali Sahu <anjali.s@goldeneagle.ai>
2025-07-15 15:10:34 +0530

    some cors changes on app file

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api", routes);
