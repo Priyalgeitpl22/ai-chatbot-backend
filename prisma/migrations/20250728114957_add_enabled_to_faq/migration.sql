09a9232fb1256a258de25ca99c31c9b83e7a08ea
Anjali Sahu <anjali.s@goldeneagle.ai>
2025-07-29 13:37:17 +0530

    integrated an endpoint to update faq enble status

---
+++ prisma/migrations/20250728114957_add_enabled_to_faq/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "FAQ" ADD COLUMN     "enabled" BOOLEAN NOT NULL DEFAULT true;
--- src/routes/faq.routes.ts
+++ src/routes/faq.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
-import { createFAQ, getFAQsByOrgId } from '../controllers/faq.controller';
+import { createFAQ, getFAQsByOrgId, updateFAQStatus } from '../controllers/faq.controller';
 import { authMiddleware } from '../middlewares/authMiddleware';
 import { uploadFaqFile } from '../controllers/faq.controller';
 import { faqFileUpload } from '../middlewares/faqUpload.middleware';
-import {getPresignedUrlHandler} from '../controllers/faq.controller'
+import { getPresignedUrlHandler } from '../controllers/faq.controller'
 
 
 const router = Router();
 
-router.post('/create', authMiddleware, createFAQ); 
+router.post('/create', authMiddleware, createFAQ);
 router.get('/:orgId', authMiddleware, getFAQsByOrgId);
 router.post('/faq-files', authMiddleware, faqFileUpload, uploadFaqFile);
 router.get('/faq-files/presigned-url/:fileKey', authMiddleware, getPresignedUrlHandler);
+router.put('/:id', authMiddleware, updateFAQStatus);
 export default router;
