7ea495ff517d4858bccbf3ea4e3ce536a0005d96
vasu singh <vasusingh9691@gmail.com>
2025-08-18 17:14:38 +0530

    conflict resolve

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { deleteThread } from "./controllers/thread.controller";
 dotenv.config();
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api", routes);
