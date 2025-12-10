0487a374790c3ee0c34fa999f02b033cf8b392df
Preyanshi <preyanshi.soni@goldeneagle.ai>
2025-12-10 18:33:03 +0530

    added sorting in get agent

--- src/controllers/agent.controller.ts
+++ src/controllers/agent.controller.ts
@@ -87,7 +87,8 @@ export const getAgents = async (req: Request, res: Response): Promise<void> => {
 
         const agents = await prisma.user.findMany({
             where: { orgId: orgId as string,deletedAt: null },
-            select: { fullName: true, email: true, id: true, role: true, orgId: true, profilePicture: true, phone: true, schedule: true, deletedAt:true,online:true, }
+            select: { fullName: true, email: true, id: true, role: true, orgId: true, profilePicture: true, phone: true, schedule: true, deletedAt:true,online:true,},
+            orderBy: { createdAt: 'desc' }
         });
 
         if (!agents) {
